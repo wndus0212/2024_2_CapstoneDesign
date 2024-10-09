31351d88d01feacb61153cd6393adab79573683b
wndus0212 <beny020101@gmail.com>
2024-10-10 02:07:17 +0900

    DetailPage

---
+++ capstonedesign/src/components/DetailPage/Search.vue
@@ -0,0 +1,18 @@
+<template>
+    <input type="text" class="SearchBar">
+</template>
+<script>
+export default {
+    name: 'SearchBar'
+}
+</script>
+<style>
+    .SearchBar{
+        width: 600px;
+        height: 30px;
+        border-radius: 15px;
+        border: 1px solid black;
+        margin-top: 30px;
+        padding-left: 10px;
+    }
+</style>
--- capstonedesign/src/views/Detail.vue
+++ capstonedesign/src/views/Detail.vue
@@ -1,37 +1,63 @@
 <template>
     <TopNav/>
-    <div>
-      <containerBox>
-        <p>
-          Details
-        </p>
-      </containerBox>
-      <containerBox>
-        <p>
-          hello
-        </p>
-      </containerBox>
-      <containerBox>
-        <p>
-          hello
-        </p>
-      </containerBox>
+    <SearchBar/>
+    <div class="ButtonContainer">
+      <SmallButton>
+        국내
+      </SmallButton>
+      <SmallButton>
+        해외
+      </SmallButton>
+    </div>
+    <div class="ButtonContainer">
+      <SmallButton>
+        전체
+      </SmallButton>
+      <SmallButton>
+        성장 기대주
+      </SmallButton>
+      <SmallButton>
+        안정 성장주
+      </SmallButton>
+      <SmallButton>
+        고수익 저평가
+      </SmallButton>
+      <SmallButton>
+        저평가 성장주
+      </SmallButton>
+    </div>
+    <div class="ButtonContainer">
+      <select>
+        <option value="1">조건 추가</option>
+      </select>
+      <SmallButton>
+        카테고리
+      </SmallButton>
+      <SmallButton>
+        시가총액
+      </SmallButton>
     </div>
   </template>
   
   <script>
   import TopNav from '../components/Top/Top.vue'
-  import containerBox from '../components/Box.vue'
+  import SearchBar from '@/components/DetailPage/Search.vue';  
+  import SmallButton from '@/components/SmallButton.vue';
   export default {
     name: 'DetailPage',
     components: {
       TopNav,
-      containerBox
+      SearchBar,
+      SmallButton
     }
   }
   </script>
   
   <style>
-
+    .ButtonContainer{
+      display: flex;
+      gap: 5px;
+      margin: 10px;
+    }
   </style>
   
