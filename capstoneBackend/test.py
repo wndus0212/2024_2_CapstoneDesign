144426a5eb44afaea6ea0885f3e8ca55bffc6d50
wndus0212 <beny020101@gmail.com>
2024-11-20 10:54:51 +0900

    afds

--- capstoneBackend/test.py
+++ capstoneBackend/test.py
@@ -8,21 +8,4 @@ import json
 import pandas as pd
 from concurrent.futures import ThreadPoolExecutor
 
-sectors=['technology',
-         'financial-services',
-         'consumer-cyclical',
-         'healthcare',
-         'communication-services',
-         'industrials',
-         'consumer-defensive',
-         'energy',
-         'real-estate',
-         'basic-materials',
-         'utilities']
-
-market_caps=[]
-
-for sector in sectors:
-    market_caps.append(yf.Sector(sector).overview['market_weight'])
-
-print(market_caps)
+print(fdr.StockListing('ETF/KR'))
